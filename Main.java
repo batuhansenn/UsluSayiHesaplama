377e9f83dfcb1e78b75d48701090fff74abf21a2
Batuhan Şen <59296529+batuhansenn@users.noreply.github.com>
2023-03-21 02:28:31 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+import javax.net.ssl.SSLContextSpi;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int n, k, sonuc = 1;1
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Taban Sayı :");
+        n = scan.nextInt();
+        System.out.print("Kuvvet Olacak Sayı :");
+        k = scan.nextInt();
+
+        for (int i = 1; i<=k;  i++) {
+            sonuc *= n;
+        }
+        System.out.println("Sonuç :" + sonuc);
+
+    }
+
+}
